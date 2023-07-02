0358d2ba56f032f9f54b2d9e413739268d8d4c7f
Pratham131 <104488785+Pratham131@users.noreply.github.com>
2023-07-02 18:44:15 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,6 +1,18 @@
 // complete the given function
+function  checkPalindrome(str, start, end) {
+		if (str.length <= 1){
+			return true;
+		}
+		if(str[start] === str[end]){
+			return checkPalindrome(str, start+1, end-1)
+		} else { 
+			return false
+		}
+}
 
 function palindrome(str){
+	let newStr = str.replace(/\s+/g, "");
 
+	return checkPalindrome(newStr,0 , newStr.length-1);
 }
 module.exports = palindrome
